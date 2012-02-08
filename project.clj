394cd8e2620b1356fb85eb315f82d991317241b5
Mike Roberts <mroberts@drw.com>
2012-02-08 08:39:14 -0500

    refactoring, updating README

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jmx-util "1.0.0-SNAPSHOT"
+(defproject jmx-util "1.0.0"
   :description "Command line access for JMX"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jmx "0.1"]
--- src/jmx_util/core.clj
+++ src/jmx_util/core.clj
@@ -1,20 +1,23 @@
 (ns jmx-util.core
-  (:require clojure.java.jmx clojure.tools.cli))
+  (:require clojure.java.jmx clojure.tools.cli)
+  (:gen-class))
 
-(defn run-with [host port bean action]
+(defn invoke [{:keys [host port bean action]}]
   (println (str "Invoking action " action " on bean " bean " at " host ":" port))
   (clojure.java.jmx/with-connection {:host host :port port}
     (clojure.java.jmx/invoke bean action)))
 
+(defn run-with [[options args banner]]
+  (when (:help options)
+    (println banner)
+    (System/exit 1))
+  (invoke options))
+
 (defn -main [& args]
-  (let [[options args banner] (clojure.tools.cli/cli args
+  (run-with (clojure.tools.cli/cli args
     ["-h" "--host" "Host to connect to" :default "localhost"]
     ["-p" "--port" "Port to connect to" :parse-fn #(Integer. %)]
     ["-b" "--bean" "Bean name"]
     ["-a" "--action" "Action name"]
-    ["-h" "--help" "Show help" :default false :flag true])]
-    (when (:help options)
-      (println banner)
-      (System/exit 1))
-    (run-with (:host options) (:port options) (:bean options) (:action options))))
+    ["--help" "Show help" :default false :flag true])))
 
